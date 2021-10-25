a7e96d7a8496477ff4a9aba0c315daedc5c28aa0
“Rebecca <hli623@wisc.edu>
2021-10-25 11:48:50 -0500

    m2

--- app/src/main/java/com/example/lab5_persistentstorage/MainActivity.java
+++ app/src/main/java/com/example/lab5_persistentstorage/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.lab5_persistentstorage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        String usernameKey = "username";
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.lab5_persistentstorage", Context.MODE_PRIVATE);
+
+        if (!sharedPreferences.getString(usernameKey, "").equals("")) {
+            String username = sharedPreferences.getString(usernameKey, "");
+            Intent intent = new Intent(this, MainActivity2.class);
+            intent.putExtra(usernameKey, username);
+            startActivity(intent);
+        } else {
+            setContentView(R.layout.activity_main);
+        }
     }
+
+    public void goToActivity2(View view) {
+        EditText usernameTextField = (EditText) findViewById(R.id.usernameText);
+        String usernameStr = usernameTextField.getText().toString();
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.lab5_persistentstorage", Context.MODE_PRIVATE);
+        sharedPreferences.edit().putString("username", usernameStr).apply();
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("username", usernameStr);
+        startActivity(intent);
+    }
+
+
 }
---
+++ app/src/main/java/com/example/lab5_persistentstorage/MainActivity3.java
@@ -0,0 +1,71 @@
+package com.example.lab5_persistentstorage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    int noteid = -1;
+    String content;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+        EditText editText = (EditText) findViewById(R.id.textBox);
+        Intent intent = getIntent();
+        noteid = intent.getIntExtra("noteid", -1);
+
+        if (noteid != -1) {
+            Note note = MainActivity2.notes.get(noteid);
+            String noteContent = note.getContent();
+            editText.setText(noteContent);
+        }
+    }
+
+    public void saveMethod (View view) {
+
+        // 1
+        EditText editTextNote = (EditText) findViewById(R.id.textBox);
+        content = editTextNote.getText().toString();
+
+        // 2
+        Context context = getApplicationContext();
+        SQLiteDatabase sqLiteDatabase = context.openOrCreateDatabase("notes", Context.MODE_PRIVATE, null);
+
+        // 3
+        DBHelper dbHelper = new DBHelper(sqLiteDatabase);
+
+        // 4
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.example.lab5_persistentstorage", Context.MODE_PRIVATE);
+        String username = sharedPreferences.getString("username", "");
+
+        String title;
+        DateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        String date = dateFormat.format(new Date());
+
+        if (noteid == -1) {
+            title = "NOTE_" + (MainActivity2.notes.size() + 1);
+            dbHelper.saveNotes(username, title, content, date);
+        } else {
+            title = "NOTE_" + (noteid + 1);
+            dbHelper.updateNote(title, date, content, username);
+        }
+
+        Intent intent = new Intent(this, MainActivity2.class);
+        intent.putExtra("username", username);
+        startActivity(intent);
+    }
+}
